23369367adb3e8f7bcf24f27aa4790e9b6734a37
Nadia Spasova <61921631+Elpida3006@users.noreply.github.com>
2020-08-27 03:54:57 +0300

    Add files via upload

---
+++ .github/workflows/Programming Basic/1. Simple Calculation/II.II. Simple Calculation MORE EX.js/5 TrainingLab.js
@@ -0,0 +1,12 @@
+
+
+function trainingLab(input) {
+    let w = weidt = Number(input.shift()) * 100;
+    let h = height = Number(input.shift()) * 100 - 100;
+    let a = 70;
+    let b = 120;
+    let work_number = ((~~(w / b)  * ~~(h / a)) - 3);
+
+
+console.log(work_number);
+}
