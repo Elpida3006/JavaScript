291d1fd476042093ff2cb5d2f083eec37281de2e
Nadq Spasova <djuzepe3006@gmail.com>
2021-01-27 19:21:27 +0200

    other views to home page
    get fns

--- .github/workflows/JS Back-End/Cubicle Part 1/controllers/homeController.js
+++ .github/workflows/JS Back-End/Cubicle Part 1/controllers/homeController.js
@@ -5,5 +5,14 @@ const router = Router();
 router.get('/', (req, res) => {
     res.redirect('/products');
 });
+router.get('/products', (req, res) => {
+    res.render('home');
+})
+router.get('/create', (req, res) => {
+    res.render('create')
+})
+router.get('/about', (req, res) => {
+    res.render('about');
+})
 
 module.exports = router;
--- .github/workflows/JS Back-End/Cubicle Part 1/controllers/productsController.js
+++ .github/workflows/JS Back-End/Cubicle Part 1/controllers/productsController.js
@@ -4,9 +4,9 @@ const router = Router();
 
 const service = require('../services/productServise');
 
-router.get('/', (req, res) => {
-    let products = service.searchByFilter(req.query);
-    res.render('home', { title: 'Products', products })
-});
+// router.get('/', (req, res) => {
+//     let products = service.searchByFilter(req.query);
+//     res.render('home', { title: 'BROWSE', products })
+// });
 
 module.exports = router;
